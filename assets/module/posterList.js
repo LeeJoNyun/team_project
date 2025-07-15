1f8b11c9a9523f48d326764dc8d7fb2cea88ed3f
jlee <115512931+LeeJoNyun@users.noreply.github.com>
2025-07-15 11:14:01 +0900

    수정

--- assets/module/posterList.js
+++ assets/module/posterList.js
@@ -1,10 +1,10 @@
 export const posters = [
-    { title: "두근두근 랜덤 책 뽑기", area: "은천", date: "2025.07.01 ~ 2025.07.01", place: "은천동작은도서관 어린이자료실", img: "../assets/images/main_5/Poster_1.png" },
-    { title: "[난향동] 뜰안에 공유서가", area: "동주민센터", date: "2025.07.04 ~ 2025.11.30", place: "난향동 주민센터 4층", img: "../assets/images/main_5/Poster_2.png" },
-    { title: "(  )시원하다", area: "성현", date: "2025.07.06 ~ 2025.08.06", place: "성현동작은도서관", img: "../assets/images/main_5/Poster_3.png" },
-    { title: "우리 그림책 읽자!", area: "은천", date: "2025.07.11 ~ 2025.09.01", place: "은천동작은도서관 어린이자료실 유아방", img: "../assets/images/main_5/Poster_4.png" },
-    { title: "[봉현] 나를 이해하는 중입니다", area: "자치회관", date: "2025.07.18 ~ 2025.10.31", place: "봉현작은도서관", img: "../assets/images/main_5/Poster_5.png" },
-    { title: "파랑아 놀자 : 여름방학 대소동", area: "관악중앙", date: "2025.07.20 ~ 2025.08.31", place: "관악아트홀 어린이라운지", img: "../assets/images/main_5/Poster_6.png" },
-    { title: "도서관 요리학교", area: "은천", date: "2025.07.01 ~ 2025.07.01", place: "은천동작은도서관 어린이자료실", img: "../assets/images/main_5/Poster_7.png" },
-    { title: "책이 활짝!", area: "관악중앙", date: "2025.07.28 ~ 2025.08.31", place: "관악중앙도서관 어린이자료실", img: "../assets/images/main_5/Poster_8.png" },
+    { title: "두근두근 랜덤 책 뽑기", area: "은천", date: "2025.07.01 ~ 2025.07.01", place: "은천동작은도서관 어린이자료실", img: "./assets/images/main_5/Poster_1.png" },
+    { title: "[난향동] 뜰안에 공유서가", area: "동주민센터", date: "2025.07.04 ~ 2025.11.30", place: "난향동 주민센터 4층", img: "./assets/images/main_5/Poster_2.png" },
+    { title: "(  )시원하다", area: "성현", date: "2025.07.06 ~ 2025.08.06", place: "성현동작은도서관", img: "./assets/images/main_5/Poster_3.png" },
+    { title: "우리 그림책 읽자!", area: "은천", date: "2025.07.11 ~ 2025.09.01", place: "은천동작은도서관 어린이자료실 유아방", img: "./assets/images/main_5/Poster_4.png" },
+    { title: "[봉현] 나를 이해하는 중입니다", area: "자치회관", date: "2025.07.18 ~ 2025.10.31", place: "봉현작은도서관", img: "./assets/images/main_5/Poster_5.png" },
+    { title: "파랑아 놀자 : 여름방학 대소동", area: "관악중앙", date: "2025.07.20 ~ 2025.08.31", place: "관악아트홀 어린이라운지", img: "./assets/images/main_5/Poster_6.png" },
+    { title: "도서관 요리학교", area: "은천", date: "2025.07.01 ~ 2025.07.01", place: "은천동작은도서관 어린이자료실", img: "./assets/images/main_5/Poster_7.png" },
+    { title: "책이 활짝!", area: "관악중앙", date: "2025.07.28 ~ 2025.08.31", place: "관악중앙도서관 어린이자료실", img: "./assets/images/main_5/Poster_8.png" },
 ]
